d294d8a9473b016f1b8cee8688dd86f168f16d6d
juan kuas <jkuasnosky@itba.edu.ar>
2022-10-09 15:43:32 -0300

    n

---
+++ Clase 10/javaspriptintro.js
@@ -0,0 +1,56 @@
+console.log("hola") // comentarios
+
+/* comentarios
+
+multilinea!
+*/
+
+console.log(1+1)
+
+/* para asignar variables:*/
+
+let nombre = juan // let , puedo reescribir la variable. HOY SE USA TODO CON LET.
+
+var apellido = kuas // var es global, si pongo var, desp nunca mas puedo reescribir la variable. No recomienda usarlo. 
+
+const edad = 20 // para asignar variables contantes, siempre va a valer eso. si le sumo 1, igual da 20 o tira error. 
+
+/* ==> conviene usar solo let y const
+importante
+*/
+
+// camelCase,      Piazza recomienda usar este estilo para asignar variables..... buena pracica, usar camelcase
+
+//snake_case
+// kebab-case
+//l33t
+
+//TODOMAYSCULA     para las "const", constantes.
+
+
+// comandos copados de tipo modal de boostrap.:
+
+alert("algo inteligente") // comando de salida
+
+let nombreusuario = prompt("ingrese su nombre:" ) // comando de entrada - input.
+
+alert("Su nombre es "+ nombreusuario)
+
+// Tipos de datos, js es re gd con los tipos de datos.:
+// Nan, not a numer
+
+// true - false.
+// Truthy y falsy. 
+
+
+// + - * / 
+
+console.log(1+1)
+console.log(1-1)
+console.log(1*1)
+console.log(1/1)
+
+console.log("juan"+"kuas")
+console.log("11"-1)
+console.log("12"+1)
+console.log()
