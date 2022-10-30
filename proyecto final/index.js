9199494dbe59b3a2c9c4a3647635251d1e5417a7
juan kuas <jkuasnosky@itba.edu.ar>
2022-10-30 18:18:25 -0300

    s

---
+++ proyecto final/index.js
@@ -0,0 +1,14 @@
+var headerbutns = document.getElementById("headerbutns")
+console.log(headerbutns)
+
+window.onscroll = function(headerbutns){
+    var top = window.scrollY;
+    console.log(top);
+
+    if (top>=880){
+        headerbutns.style.id="headerbutnsactive" 
+    };
+    
+}
+
+
