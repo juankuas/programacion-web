837288eb981bbdf4e3a0ce19330472bd78169bb0
juan kuas <jkuasnosky@itba.edu.ar>
2022-11-06 20:52:33 -0300

    n

--- proyecto final/iflogin==true/1-Indumentaria/induyguardadologg.js
+++ proyecto final/iflogin==true/1-Indumentaria/induyguardadologg.js
@@ -14,27 +14,52 @@ const favoitoscontainer = document.querySelector(".favoitoscontainer");
 
 const botonguardado = document.querySelectorAll(".botonguardado");
 botonguardado.forEach(addtofavoritos => {
-    addtofavoritos.addEventListener('click', addToFavoritosClickedCompu)
+    addtofavoritos.addEventListener('click', addToFavoritosClickedCompu, addToFavoritosClickedCelu)
 })
 
 function addToFavoritosClickedCompu(event){
     const button = event.target;
     const card = button.closest('.card')
-    const cardcelu = button.closest('.cardcelu')
     console.log(card)
-    console.log(cardcelu)
 
     const img = card.querySelector('.card-img-top').src;
     const cardtitle = card.querySelector('.card-title').textContent;
     const cardtext = card.querySelector('.card-text').textContent;
 
+    var addfav = card.querySelector('.paddfavs').className = "paddfavsNOT"
 
 
-    addcardtofavoritosCompu(img, cardtitle, cardtext)
+
+
+
+    addcardtofavoritosCompu(img, cardtitle, cardtext, addfav)
 
 }
 
+// celu
+function addToFavoritosClickedCelu(event){
+    const button = event.target;
+    const cardcelu = button.closest('.cardcelu')
+    console.log(cardcelu)
+
+    const imgcelu = card.querySelector('.imgindumentaria').src;
+    const titlemodal = card.querySelector('.lead').textContent;
+    const pmodalcelu = card.querySelector('.pmodalcelu').textContent;
+
+    var addfav = card.querySelector('.paddfavs').className = "paddfavsNOT"
+
+
+
+
+
+    addcardtofavoritosCompu(imgcelu, titlemodal, pmodalcelu, addfav)
+}
+//
+
 function addcardtofavoritosCompu(img, cardtitle, cardtext){
+
+
+
     console.log(img)
     console.log( cardtitle )
     console.log(cardtext )
@@ -50,7 +75,7 @@ function addcardtofavoritosCompu(img, cardtitle, cardtext){
 
           <p class="card-text">${cardtext}</p>
           <a id="webinstabut" href="https://undefinedba.com" class="btn btn-outline-primary" target="_blank"><p id="webinstap">Sitio Web</p></a>
-          <a id="webinstabut" href="https://www.instagram.com/undefined_ba/" class="btn btn-outline-primary" target="_blank"><p id="webinstap">Instagram</p></a>
+          <a id="webinstabut" href="" class="btn btn-outline-primary" target="_blank"><p id="webinstap">Instagram</p></a>
           <button class="btn_eliminar_de_favs"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square-fill" viewBox="0 0 16 16">
           <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"/>
         </svg></button>
@@ -64,7 +89,10 @@ function addcardtofavoritosCompu(img, cardtitle, cardtext){
     favoritoscard.innerHTML = favoritosCardContent;
     favoitoscontainer.append(favoritoscard);
 
-    favoritoscard.querySelector('.btn_eliminar_de_favs').addEventListener('click', deleteCardFav)
+    favoritoscard.querySelector('.btn_eliminar_de_favs').addEventListener('click', deleteCardFav, changeclassPADDFAVS)
+
+    
+
     
 }
 
@@ -72,6 +100,14 @@ function addcardtofavoritosCompu(img, cardtitle, cardtext){
 function deleteCardFav(event){
     const buttonCliked = event.target;
     buttonCliked.closest('.card').remove();
+
 }
+
+function changeclassPADDFAVS(addfav){
+    addfav.className="paddfavs"
+    
+}
+
+
 
 
